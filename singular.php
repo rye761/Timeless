f2967d305a2e6a79d75030218d2504f9b4d6faf2
rye761 <ryebread761@gmail.com>
2017-08-08 17:31:10 -0600

    Singular

--- singular.php
+++ singular.php
@@ -7,18 +7,18 @@ get_header();
     while (have_posts()) :
       the_post();
       ?>
-        <article class="post">
+        <article class="singular-post">
           <h2><a class="title" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
           <h3 class="post-info"><?php the_time('l, F j, Y'); ?></h3>
-          <?php the_content(__('Continue Reading').'&rarr;'); ?>
+          <?php $singlecolumn = (count(explode(" ", get_the_content())) < 100) ? 'single-column' : ''; ?>
+          <div class="singular-post-content <?php echo $singlecolumn ?>">
+            <?php the_content(); ?>
+          </div>
         </article>
       <?php
     endwhile;
   endif;
   ?>
-</div>
-<div class="navigation-links">
-  <?php echo paginate_links(); ?>
 </div>
 <?php
 get_sidebar();
