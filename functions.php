9f74668e5ee02b44fea35c16667190f6025e877e
rye761 <ryebread761@gmail.com>
2018-04-05 22:11:31 -0600

    basic sidebar

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@ add_action('init', 'register_menu');
 add_action('wp_enqueue_scripts', 'timeless_scripts');
 add_action('after_setup_theme', 'timeless_setup');
 add_action('pre_get_posts', 'timeless_post_query');
+add_action('widgets_init', 'timeless_widgets_init');
 
 function register_menu() {
 	register_nav_menu('primary-menu', __('Primary Menu'));
@@ -22,6 +23,18 @@ function timeless_post_query($query) {
 	if (is_home() && $query->is_main_query()) {
 		$query->set('posts_per_page', 3);
 	}
+}
+
+function timeless_widgets_init() {
+	register_sidebar( array(
+		'name'          => __('Widget Area'),
+		'id'            => 'sidebar-1',
+		'description'   => __('Add widgets here to appear in your sidebar.'),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
 }
 
 function timeless_excerpt_length() {
