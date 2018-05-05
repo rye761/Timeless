f7f37048d3ffc852201084232108fa76953cf5a4
rye761 <ryebread761@gmail.com>
2018-05-04 21:49:17 -0600

    meta description

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="<?php bloginfo('description'); ?>">
 
     <?php wp_head(); ?>
   </head>
